612c13166567b0b0ac05f4b1a3a0bff8ff0554d1
jacklayfield <layf100@gmail.com>
2024-01-04 21:36:44 -0500

    Added http/io to server, created tasks

---
+++ README.md
@@ -0,0 +1,11 @@
+## Packster
+
+#### A simple group packing app
+
+TODO:
+
+- [ ] Decide between mongoose or mongodb driver (mongoose since most data will be structured, but mongodb driver could also work as long as some sort of schema is eforced)
+- [x] Set up server in it's basic form (express, http, io)
+- [ ] Design and create model for a "item" entry for the items list
+- [ ] Implement frontend to create a "room" with a unique id
+- [ ] Work on join room event for a ids
--- server/index.mjs
+++ server/index.mjs
@@ -3,6 +3,8 @@ import cors from "cors";
 import "./loadEnv.mjs";
 import "express-async-errors";
 import test from "./routes/test.mjs";
+import http from "http";
+import { Server } from "socket.io";
 
 console.log("MONGOURL", process.env.MONGO_URL);
 
@@ -18,6 +20,22 @@ app.use("/test", test);
 // Global error handling
 app.use((err, _req, res, next) => {
   res.status(500).send("Uh oh! An unexpected error occured.");
+});
+
+//create an http server using express app
+const server = http.createServer(app);
+
+// create io server
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+// io logic
+io.on("connection", (socket) => {
+  socket.emit("connect", { message: "a new client connected" });
 });
 
 // start the Express server
